63e5b7e113eb8be49462252607d80a766b5b7e45
Ryan <ryanrigato@gmail.com>
2020-06-06 09:34:38 -0500

    create dynamodb table resource

--- scripts/reddit_ratings.py
+++ scripts/reddit_ratings.py
@@ -60,6 +60,10 @@ def get_boto_clients(resource_name, region_name='us-east-1',
         service_client : boto3.client
             boto3 client for the aws resource in resource_name
             in region region_name
+
+        dynamodb_table_resource : boto3.resource.Table
+            boto3 Table resource, only returned if table_name is
+            not None
         
 
 
@@ -72,6 +76,16 @@ def get_boto_clients(resource_name, region_name='us-east-1',
             service_name=resource_name, 
             region_name=region_name
         )
+
+    '''
+        return boto3 DynamoDb table resource in addition to boto3 client
+        if table_name parameter is not None
+    '''
+    if table_name is not None:
+        dynamodb_table_resource = boto3.resource(
+            service_name=resource_name,
+            region_name=region_name
+        ).Table(table_name)
     return(service_client)
 
 
