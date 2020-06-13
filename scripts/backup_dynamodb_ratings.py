5702efd54d4026ce1ee67d4494d62fecb81a965d
Ryan <ryanrigato@gmail.com>
2020-06-13 11:01:35 -0500

    stub for lambda function to backup ratings

---
+++ scripts/backup_dynamodb_ratings.py
@@ -0,0 +1,150 @@
+from boto3.dynamodb import conditions
+from bs4 import BeautifulSoup
+from datetime import datetime
+from dateutil import parser
+
+import boto3
+import json
+import logging
+import math
+import os
+import requests
+
+'''
+    Special user agent that is recommended according to the
+    api docs
+    <platform>:<app ID>:<version string> (by /u/<reddit username>)
+'''
+REDDIT_USER_AGENT = "Lambda:toonamiratings:v1.0 (by /u/toonamiratings)"
+
+
+def get_logger(working_directory=os.getcwd()):
+    """Sets up logger
+
+        Parameters
+        ----------
+        working_directory: str
+            Where to put logger, defaults to cwd
+
+        Returns
+        -------
+
+        Raises
+        ------
+    """
+   
+    '''
+        Adds the file name to the logs/ directory without
+        the extension
+    '''
+    logging.basicConfig(
+        filename=os.path.join(working_directory, "logs/",
+        os.path.basename(__file__).split(".")[0]),
+        format="%(asctime)s %(message)s",
+         datefmt="%m/%d/%Y %I:%M:%S %p", level=logging.DEBUG
+         )
+    logging.info("\n")
+
+def get_boto_clients(resource_name, region_name='us-east-1',
+    table_name=None):
+    '''Returns the boto client for various aws resources
+
+        Parameters
+        ----------
+        resource_name : str
+            Name of the resource for the client
+
+        region_name : str
+                aws region you are using, defaults to
+                us-east-1
+
+        Returns
+        -------
+        service_client : boto3.client
+            boto3 client for the aws resource in resource_name
+            in region region_name
+
+        dynamodb_table_resource : boto3.resource.Table
+            boto3 Table resource, only returned if table_name is
+            not None
+        
+
+
+        Raises
+        ------
+    '''
+
+    service_client = boto3.client(
+            service_name=resource_name, 
+            region_name=region_name
+        )
+
+    '''
+        return boto3 DynamoDb table resource in addition to boto3 client
+        if table_name parameter is not None
+    '''
+    if table_name is not None:
+        dynamodb_table_resource = boto3.resource(
+            service_name=resource_name,
+            region_name=region_name
+        ).Table(table_name)
+
+        return(service_client, dynamodb_table_resource)
+
+    '''
+        Otherwise return just a resource client
+    '''
+    return(service_client)
+
+
+
+def lambda_handler(event, context):
+    """Handles lambda invocation from cloudwatch events rule
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        Raises
+        ------
+    """
+    '''
+        Logging required for cloudwatch logs
+    '''
+    logging.getLogger().setLevel(logging.INFO)
+    main()
+
+
+def main():
+    """Entry point into the script
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        Raises
+        ------
+    """
+
+    all_ratings_list = ratings_iteration(number_posts=25)
+
+    clean_rating_keys = dict_key_mapping(
+        pre_clean_ratings_keys=all_ratings_list
+    )
+
+    clean_rating_values = clean_dict_value(
+        ratings_values_to_clean=all_ratings_list
+    )
+
+
+    handle_ratings_insertion(
+        all_ratings_list=all_ratings_list,
+        table_name=os.environ.get("DYNAMODB_TABLE_NAME")
+    )
+
+if __name__ == "__main__":
+    get_logger()    
+    main()
