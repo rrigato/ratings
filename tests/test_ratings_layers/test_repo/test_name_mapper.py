a13be069818f7b7a12d56f27b979a8926376b500
Ryan <ryanrigato@gmail.com>
2022-02-27 10:38:46 -0600

    extracting a name wrapper

---
+++ tests/test_ratings_layers/test_repo/test_name_mapper.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestNameMapper(unittest.TestCase):
+    def test_get_table_column_name_mapping(self):
+        """Each column name mapping value for a ratings post is a str"""
+        from ratings_layers.repo.name_mapper import get_table_column_name_mapping
+        
+        self.assertGreater(
+            len(get_table_column_name_mapping().keys()),
+            10
+        )
+        for column_name in get_table_column_name_mapping().keys():
+            self.assertEqual(
+                type(get_table_column_name_mapping()[column_name]),
+                str
+            )
+
+    @unittest.skip("skipping for now")
+    def test_get_table_column_name_mapping_viewer_e2e(self):
+        """Bug where viewer key was not mapped"""
+        pass
