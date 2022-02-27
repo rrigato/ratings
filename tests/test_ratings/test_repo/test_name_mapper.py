eb6f12fc4c10db64575e7f07251f34c746a93d91
Ryan <ryanrigato@gmail.com>
2022-02-27 11:06:10 -0600

    naming conventions

--- tests/test_ratings/test_repo/test_name_mapper.py
+++ tests/test_ratings/test_repo/test_name_mapper.py
@@ -3,7 +3,7 @@ import unittest
 class TestNameMapper(unittest.TestCase):
     def test_get_table_column_name_mapping(self):
         """Each column name mapping value for a ratings post is a str"""
-        from ratings_layers.repo.name_mapper import get_table_column_name_mapping
+        from ratings.repo.name_mapper import get_table_column_name_mapping
         
         self.assertGreater(
             len(get_table_column_name_mapping().keys()),
@@ -17,7 +17,7 @@ class TestNameMapper(unittest.TestCase):
 
     def test_get_table_column_name_mapping_viewers_e2e(self):
         """Bug where viewers key was not mapped"""
-        from ratings_layers.repo.name_mapper import get_table_column_name_mapping
+        from ratings.repo.name_mapper import get_table_column_name_mapping
 
         self.assertEqual(
             type(get_table_column_name_mapping()["viewers"]),
