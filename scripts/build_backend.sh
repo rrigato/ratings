f4921398d9c92ac661855c78dee11a74237c902a
rrigato <ryanrigato@gmail.com>
2022-07-30 13:52:02 -0500

    first pass at lambda deployment upgrade to python version 3.9

--- scripts/build_backend.sh
+++ scripts/build_backend.sh
@@ -2,3 +2,15 @@
 
 #exits program immediately if a command is not sucessful
 set -e
+
+pip install --target ./deployment requests
+pip install --target ./deployment bs4
+
+cp -r ratings deploypkg
+cp scripts/reddit_ratings.py deploypkg
+
+cd deployment
+
+zip -r9 built_lambda.zip .
+
+cd ..
