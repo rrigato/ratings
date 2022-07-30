2e1c445c22501a689a78ccc341f6b554d4a73dcd
rrigato <ryanrigato@gmail.com>
2022-07-30 15:03:08 -0500

    able to update lambda function with build script

--- scripts/build_backend.sh
+++ scripts/build_backend.sh
@@ -3,14 +3,26 @@
 #exits program immediately if a command is not sucessful
 set -e
 
+LAMBDA_FUNCTION_NAME="prod-ratings-backend-lambda-poll"
+
 pip install --target ./deployment requests
 pip install --target ./deployment bs4
 
-cp -r ratings deploypkg
-cp scripts/reddit_ratings.py deploypkg
+cp -r ratings deployment
+cp scripts/reddit_ratings.py deployment
 
 cd deployment
 
 zip -r9 built_lambda.zip .
 
 cd ..
+
+
+aws lambda update-function-code --region us-east-1 \
+--function-name $LAMBDA_FUNCTION_NAME \
+--zip-file fileb://deployment/built_lambda.zip
+
+aws lambda update-function-configuration --region us-east-1 \
+--function-name $LAMBDA_FUNCTION_NAME \
+--handler "reddit_ratings.lambda_handler" \
+--no-cli-pager
