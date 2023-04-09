2255ec7a51c49e1b970e0cde9ca0079822756fc6
rrigato <ryanrigato@gmail.com>
2023-04-09 12:20:31 -0500

    load_secret_config implementation

--- ratings/repo/ratings_repo_backend.py
+++ ratings/repo/ratings_repo_backend.py
@@ -1,8 +1,11 @@
-from copy import deepcopy
+import json
 import logging
-from typing import List, Optional, Union
+from copy import deepcopy
+from typing import Dict, List, Optional, Union
 
-from ratings.entities.ratings_entities import TelevisionRating
+import boto3
+
+from ratings.entities.ratings_entities import SecretConfig, TelevisionRating
 
 
 def ratings_from_internet() -> Union[
@@ -13,4 +16,56 @@ def ratings_from_internet() -> Union[
     None, str if error
     """
     return(None)
+
+def _populate_secret_config(sdk_response: Dict) -> SecretConfig:
+    """https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/secretsmanager.html#SecretsManager.Client.get_secret_value
+    """
+    secret_config = SecretConfig()
+
+    deserialized_secret_string = json.loads(
+        sdk_response["SecretString"]
+    )
+
+    logging.info(f"_populate_secret_config - deserialized secrets")
+    
+    secret_config.reddit_client_id = deserialized_secret_string[
+        "reddit_api_key"
+    ]
+    
+    secret_config.reddit_client_secret = deserialized_secret_string[
+        "reddit_api_secret"
+    ]
+    secret_config.reddit_password = deserialized_secret_string[
+        "reddit_api_password"
+    ]
+    secret_config.reddit_username = deserialized_secret_string[
+        "reddit_api_username"
+    ]
+    
+    return(secret_config)
+
+
+
+def load_secret_config() -> Optional[SecretConfig]:
+    """Returns None if unexpected error
+    """
+    try:
+        
+        secretsmanager_client = boto3.client(
+            service_name="secretsmanager", 
+            region_name="us-east-1"
+        )
+
+        sdk_secret_response = secretsmanager_client.get_secret_value(
+            SecretId="/prod/v1/credentials"
+        )
+        logging.info(f"load_secret_config - obtained config")
+        
+
+        return(_populate_secret_config(
+            sdk_secret_response
+        ))
+    except Exception as errror_suppression:
+        logging.exception(errror_suppression)
+        return(None)
 
