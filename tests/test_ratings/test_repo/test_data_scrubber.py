c56a21367c6834a376c88ab528716161f903fbb5
rrigato <ryanrigato@gmail.com>
2023-05-29 09:12:16 -0500

    remove _manual_skip_date

--- tests/test_ratings/test_repo/test_data_scrubber.py
+++ tests/test_ratings/test_repo/test_data_scrubber.py
@@ -22,9 +22,7 @@ class TestDataScrubber(unittest.TestCase):
         data_override_factory(all_ratings_list=ratings_fixture_bad_data()[0:2])
 
 
-        manual_skip_date_mock.assert_called_once_with(
-            ratings_date_to_skip="2022-01-29",
-            all_ratings_list=ratings_fixture_bad_data()[0:2]
+        manual_skip_date_mock.assert_not_called(
         )
 
         override_ratings_occurred_on_mock.assert_called_once_with(
@@ -38,46 +36,6 @@ class TestDataScrubber(unittest.TestCase):
         )
 
         manual_override_by_date_mock.assert_called()
-
-
-    def test_manual_skip_date(self):
-        """17 matching ratings elements removed"""
-        from fixtures.get_all_ratings_list import ratings_fixture_bad_data
-        from ratings.repo.data_scrubber import _manual_skip_date
-
-        mock_ratings_list = ratings_fixture_bad_data()
-
-
-        _manual_skip_date(
-            ratings_date_to_skip="2022-01-29",
-            all_ratings_list=mock_ratings_list
-        )
-
-
-        self.assertEqual(
-            len(mock_ratings_list), 
-            len(ratings_fixture_bad_data()) - 17
-        )
-
-
-    def test_manual_skip_date_out_of_index(self):
-        """Drop elements from end of list to avoid IndexError"""
-        from fixtures.get_all_ratings_list import ratings_fixture_bad_data
-        from ratings.repo.data_scrubber import _manual_skip_date
-
-        mock_ratings_list = ratings_fixture_bad_data()
-
-
-        _manual_skip_date(
-            ratings_date_to_skip="2022-01-15",
-            all_ratings_list=mock_ratings_list
-        )
-
-
-        self.assertEqual(
-            len(mock_ratings_list), 
-            len(ratings_fixture_bad_data()) - 8
-        )
 
 
     def test_override_ratings_occurred_on(self):
