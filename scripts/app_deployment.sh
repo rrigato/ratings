918a1d0ae36cd3e020740cdefbf53a4c9d95acc0
rrigato <ryanrigato@gmail.com>
2023-08-26 08:44:44 -0500

    remove lambda function update from deployment

--- scripts/app_deployment.sh
+++ scripts/app_deployment.sh
@@ -3,7 +3,6 @@
 #exits program immediately if a command is not sucessful
 set -e
 
-LAMBDA_FUNCTION_NAME="prod-ratings-backend-lambda-poll"
 
 secret_scan_results=$(detect-secrets scan | \
 python3 -c "import sys, json; print(json.load(sys.stdin)['results'])" )
@@ -32,26 +31,7 @@ zip -r9 built_lambda.zip . \
 cd ..
 
 
-
-aws lambda update-function-configuration --region us-east-1 \
---function-name $LAMBDA_FUNCTION_NAME \
---no-cli-pager \
---handler "reddit_ratings.lambda_handler" 
-
-aws lambda wait function-updated --region us-east-1 \
---function-name $LAMBDA_FUNCTION_NAME
-
 echo "---------------completed handler update---------------------"
-
-aws lambda update-function-code --region us-east-1 \
---function-name $LAMBDA_FUNCTION_NAME \
---no-cli-pager \
---zip-file fileb://deployment/built_lambda.zip
-
-aws lambda wait function-updated --region us-east-1 \
---function-name $LAMBDA_FUNCTION_NAME
-
-
 
 #cleanup
 rm -r deployment
